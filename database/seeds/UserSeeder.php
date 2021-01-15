e5a0a76820dca9edd74ab4c1e582bf106c1870f7
BazioDEV <bazio.apple@gmail.com>
2021-01-16 01:05:47 +0700

    first commit

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+        User::create([
+            'name'  => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
+        ]);
+        $this->command->info('User account created with following details: admin@admin.com, password');
+    }
+}
